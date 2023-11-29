d9a5c1d621352574cbf7c7b2bc9f2ab9694883c8
Saplyn-Edifice <saplynx@outlook.com>
2023-11-29 21:47:23 +0800

    Initial commit

---
+++ backend/src/main/java/org/lynx/noisy/NoisyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.lynx.noisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoisyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoisyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/lynx/noisy/NoisyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lynx.noisy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoisyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
